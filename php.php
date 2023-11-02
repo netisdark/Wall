3603d9b8deeb1129a384d37907f9e1cb5c82c686
netisdark <94221891+netisdark@users.noreply.github.com>
2023-11-02 13:32:12 +0545

    Add files via upload

---
+++ php.php
@@ -0,0 +1,13 @@
+<?php
+      $echo "hello world";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <title>Page title</title>
+</head>
+<body>
+      
+</body>
+</html>
